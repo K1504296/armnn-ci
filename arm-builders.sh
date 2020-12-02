b6f9a5bb5b243026d8d6966c78492c50b57e6b45
Theodore <greytrt@gmail.com>
2020-12-02 13:29:18 +0000

    Update arm-builders.sh
    removing boost build and adding apt-get install libboost-dev.

--- arm-builders.sh
+++ arm-builders.sh
@@ -4,7 +4,7 @@ set -ex
 
 sudo apt -q=2 update
 sudo apt -q=2 install -y --no-install-recommends build-essential scons cmake git autoconf automake curl libtool libpthread-stubs0-dev
-sudo apt -q=2 install -y --no-install-recommends python3-pip virtualenv python3-dev python3-setuptools
+sudo apt -q=2 install -y --no-install-recommends python3-pip virtualenv python3-dev python3-setuptools libboost-dev
 pip3 install wheel
 # Set local configuration
 git config --global user.email "ci_notify@linaro.org"
@@ -17,7 +17,6 @@ git clone "https://review.mlplatform.org/ml/armnn"
 git clone --depth 1 https://github.com/protocolbuffers/protobuf.git --branch v3.9.2 --single-branch
 git clone --depth 1 https://github.com/tensorflow/tensorflow.git --branch r2.0 --single-branch
 git clone --depth 1 https://github.com/google/flatbuffers.git --branch v1.12.0 --single-branch
-wget -q https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2 && tar xf boost_*.tar.bz2
 #swig 4.0
 wget -q http://prdownloads.sourceforge.net/swig/swig-4.0.2.tar.gz
 #export ONNX_ML=1
@@ -55,16 +54,6 @@ cd ${WORKSPACE}/ComputeLibrary
 #need to add if loops for opencl=1 embed_kernels=1 and neon=1
 scons -u -j$(nproc) arch=arm64-v8a extra_cxx_flags="-fPIC" benchmark_tests=1 validation_tests=1 embed_kernels=1
 
-#build Boost
-cd ${WORKSPACE}/boost_1_64_0
-./bootstrap.sh
-./b2  \
-  --build-dir=${WORKSPACE}/boost_1_64_0/build toolset=gcc link=static cxxflags=-fPIC \
-  --with-filesystem \
-  --with-test \
-  --with-log \
-  --with-program_options install --prefix=${WORKSPACE}/boost
-
 #build Protobuf
 cd ${WORKSPACE}/protobuf
 git submodule update --init --recursive
@@ -97,7 +86,6 @@ cd build
 cmake .. \
   -DARMCOMPUTE_ROOT=${WORKSPACE}/ComputeLibrary \
   -DARMCOMPUTE_BUILD_DIR=${WORKSPACE}/ComputeLibrary/build \
-  -DBOOST_ROOT=${WORKSPACE}/boost \
   -DTF_GENERATED_SOURCES=${WORKSPACE}/tensorflow-protobuf \
   -DBUILD_TF_PARSER=1 \
   -DPROTOBUF_ROOT=${WORKSPACE}/protobuf-host \
@@ -113,13 +101,13 @@ cmake .. \
 make -j$(nproc)
 
 cd ${WORKSPACE}
-rm -rf boost_*.tar.bz2 boost_* protobuf tensorflow
+rm -rf protobuf tensorflow
 find ${WORKSPACE} -type f -name *.o -delete
 tar -cJf /tmp/armnn-full.tar.xz ${WORKSPACE}
 
 mv armnn/build .
 mv protobuf-host/lib/libprotobuf.so.20.0.2 build
-rm -rf boost armnn ComputeLibrary flatbuffers protobuf-host tensorflow-protobuf builders.sh
+rm -rf armnn ComputeLibrary flatbuffers protobuf-host tensorflow-protobuf builders.sh
 tar -cJf /tmp/armnn.tar.xz ${WORKSPACE}
 
 mkdir ${WORKSPACE}/out
