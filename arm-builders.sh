4f7e5dced041bd0033b4844a23a593aebed8888b
Theodore <greytrt@gmail.com>
2020-12-14 13:19:49 +0000

    Update arm-builders.sh
    testing updates to compute lib

--- arm-builders.sh
+++ arm-builders.sh
@@ -10,7 +10,8 @@ git config --global user.name "Linaro CI"
 
 cd ${WORKSPACE}
 
-git clone --depth 1 "https://review.mlplatform.org/ml/ComputeLibrary"
+#git clone --depth 1 "https://review.mlplatform.org/ml/ComputeLibrary"
+git clone https://github.com/K1504296/ComputeLibrary.git
 git clone "https://review.mlplatform.org/ml/armnn"
 git clone --depth 1 https://github.com/protocolbuffers/protobuf.git --branch v3.9.2 --single-branch
 git clone --depth 1 https://github.com/tensorflow/tensorflow.git --branch r2.0 --single-branch
@@ -48,7 +49,7 @@ make
 make install
 
 cd ${WORKSPACE}/ComputeLibrary
-git fetch "https://review.mlplatform.org/ml/ComputeLibrary" refs/changes/30/4230/5 && git cherry-pick FETCH_HEAD
+#git fetch "https://review.mlplatform.org/ml/ComputeLibrary" refs/changes/30/4230/5 && git cherry-pick FETCH_HEAD
 #need to add for opencl=1 embed_kernels=1 and neon=1
 scons -u -j$(nproc) arch=arm64-v8a extra_cxx_flags="-fPIC" benchmark_tests=1 validation_tests=1 embed_kernels=1
 
