bbc2fffd73da7e256407e516908d3dcb395a5abd
Theodore <greytrt@gmail.com>
2021-02-22 14:10:45 +0000

    Update tvm-builders.sh
    adding -y to apt-get install

--- tvm-builders.sh
+++ tvm-builders.sh
@@ -3,7 +3,7 @@
 set -ex
 
 sudo apt -q=2 update
-sudo apt-get install llvm
+sudo apt-get -y install llvm
 pip3 install wheel
 # Set local configuration
 git config --global user.email "ci_notify@linaro.org"
